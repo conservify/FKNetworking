dff20443a3b651422008dfdc06e7d79d12a9d7ca
Jacob Lewallen <jlewallen@gmail.com>
2019-10-29 14:37:21 -0700

    Service discovery.

--- FKNetworking/Classes/Networking.swift
+++ FKNetworking/Classes/Networking.swift
@@ -11,15 +11,28 @@ import os
 
 @objc
 open class ServiceInfo : NSObject {
-    public var type: String = ""
-    public var name: String = ""
-    public var host: String = ""
-    public var port: Int = 0
+    @objc public var type: String = ""
+    @objc public var name: String = ""
+    @objc public var host: String = ""
+    @objc public var port: Int = 0
+    
+    @objc
+    public init(type: String, name: String, host: String, port: Int) {
+        self.type = type
+        self.name = name
+        self.host = host
+        self.port = port
+    }
 }
 
 @objc
 open class WifiNetwork : NSObject {
-    public var ssid: String = ""
+    @objc public var ssid: String = ""
+    
+    @objc
+    public init(ssid: String) {
+        self.ssid = ssid
+    }
 }
 
 @objc
@@ -29,28 +42,29 @@ open class WifiNetworks : NSObject {
 @objc
 public protocol NetworkingListener {
     func onStarted()
-    //func onFoundService(service: ServiceInfo)
-    //func onLostService(service: ServiceInfo)
-    //func onConnectionInfo(connected: Bool)
-    //func onConnectedNetwork(network: WifiNetwork)
-    //func onNetworksFound(networks: WifiNetworks)
-    //func onNetworkScanError()
+    func onFoundService(service: ServiceInfo)
+    func onLostService(service: ServiceInfo)
+    func onConnectionInfo(connected: Bool)
+    func onConnectedNetwork(network: WifiNetwork)
+    func onNetworksFound(networks: WifiNetworks)
+    func onNetworkScanError()
 }
 
 @objc
 public protocol WebTransferListener {
-    func onStarted(task: String, headers: [String:String])
-    func onProgress(task: String, bytes: Int, total: Int)
-    func onComplete(task: String, headers: [String:String], contentType: String, body: Any, statusCode: Int)
+    func onStarted(taskId: String, headers: [String:String])
+    func onProgress(taskId: String, bytes: Int, total: Int)
+    func onComplete(taskId: String, headers: [String:String], contentType: String, body: Any, statusCode: Int)
+    func onError(taskId: String)
 }
 
 @objc
 open class WebTransfer : NSObject {
-    var url: String? = nil
-    var path: String? = nil
-    var body: String? = nil
-    var contentType: String? = nil
-    var headers: [String: String] = [String:String]()
+    @objc var url: String? = nil
+    @objc var path: String? = nil
+    @objc var body: String? = nil
+    @objc var contentType: String? = nil
+    @objc var headers: [String: String] = [String:String]()
 }
 
 @objc
@@ -65,11 +79,6 @@ open class Web : NSObject {
         super.init()
     }
     
-    @objc
-    public func test() {
-        NSLog("Web::test")
-    }
-
     @objc
     public func json(info: WebTransfer) -> String {
         return ""
@@ -96,6 +105,8 @@ open class ServiceDiscovery : NSObject, NetServiceBrowserDelegate, NetServiceDel
     var networkingListener: NetworkingListener
     var browser: NetServiceBrowser
     
+    var pending: NetService?
+    
     @objc
     init(networkingListener: NetworkingListener) {
         self.networkingListener = networkingListener
@@ -105,21 +116,94 @@ open class ServiceDiscovery : NSObject, NetServiceBrowserDelegate, NetServiceDel
     
     @objc
     public func start(serviceType: String) {
-        NSLog("ServiceDiscovery::start");
+        NSLog("ServiceDiscovery::starting");
+        NSLog(serviceType);
+        pending = nil
         browser.delegate = self
-        browser.searchForServices(ofType: serviceType, inDomain: "")
+        browser.stop()
+        browser.searchForServices(ofType: serviceType, inDomain: "local.")
+        networkingListener.onStarted()
     }
     
-    func netServiceBrowser(aNetServiceBrowser: NetServiceBrowser!, didFindService aNetService: NetService!, moreComing: Bool) {
+    public func netServiceWillResolve(_ sender: NetService) {
+        NSLog("ServiceDiscovery::netServiceWillResolve");
     }
     
-    func netServiceBrowser(aNetServiceBrowser: NetServiceBrowser!, didRemoveService aNetService: NetService!, moreComing: Bool) {
+    public func netServiceDidResolveAddress(_ sender: NetService) {
+        NSLog("ServiceDiscovery::netServiceDidResolveAddress %@ %@", sender.name, sender.hostName ?? "<none>");
+        
+        if let serviceIp = resolveIPv4(addresses: sender.addresses!) {
+            NSLog("Found IPV4: %@", serviceIp)
+            networkingListener.onFoundService(service: ServiceInfo(type: sender.type, name: sender.name, host: serviceIp, port: sender.port))
+        }
+        else {
+            NSLog("No ipv4")
+        }
     }
     
-    func netServiceDidResolveAddress(netservice: NetService!) {
+    public func netService(_ sender: NetService, didNotResolve errorDict: [String : NSNumber]) {
+        NSLog("ServiceDiscovery::didNotResolve");
     }
     
-    func netService(netservice: NetService!, didNotResolve errorDict: [NSObject : AnyObject]!) {
+    public func netServiceBrowserWillSearch(_ browser: NetServiceBrowser) {
+        NSLog("ServiceDiscovery::willSearch")
+    }
+    
+    public func netServiceBrowserDidStopSearch(_ browser: NetServiceBrowser) {
+        NSLog("ServiceDiscovery::netServiceBrowserDidStopSearch");
+    }
+    
+    public func netServiceBrowser(_ browser: NetServiceBrowser, didNotSearch errorDict: [String : NSNumber]) {
+        NSLog("ServiceDiscovery::didNotSearch");
+    }
+    
+    public func netServiceBrowser(_ browser: NetServiceBrowser, didFindDomain domainString: String, moreComing: Bool) {
+        NSLog("ServiceDiscovery::didFindDomain");
+    }
+    
+    public func netServiceBrowser(_ browser: NetServiceBrowser, didFind service: NetService, moreComing: Bool) {
+        NSLog("ServiceDiscovery::didFindService %@ %@", service.name, service.type);
+        
+        service.stop()
+        service.delegate = self
+        service.resolve(withTimeout: 5.0)
+
+        // TODO Do we need a queue of these?
+        pending = service
+    }
+    
+    public func netServiceBrowser(_ browser: NetServiceBrowser, didRemoveDomain domainString: String, moreComing: Bool) {
+        NSLog("ServiceDiscovery::didRemoveDomain");
+    }
+    
+    public func netServiceBrowser(_ browser: NetServiceBrowser, didRemove service: NetService, moreComing: Bool) {
+        NSLog("ServiceDiscovery::didRemoveService %@", service.name);
+        networkingListener.onLostService(service: ServiceInfo(type: service.type, name: service.name, host: "", port: 0))
+    }
+    
+    func resolveIPv4(addresses: [Data]) -> String? {
+        var resolved: String?
+        
+        for address in addresses {
+            let data = address as NSData
+            var storage = sockaddr_storage()
+            data.getBytes(&storage, length: MemoryLayout<sockaddr_storage>.size)
+            
+            if Int32(storage.ss_family) == AF_INET {
+                let addr4 = withUnsafePointer(to: &storage) {
+                    $0.withMemoryRebound(to: sockaddr_in.self, capacity: 1) {
+                        $0.pointee
+                    }
+                }
+    
+                if let ip = String(cString: inet_ntoa(addr4.sin_addr), encoding: .ascii) {
+                    resolved = ip
+                    break
+                }
+            }
+        }
+        
+        return resolved
     }
 }
 
@@ -145,33 +229,6 @@ open class Networking : NSObject {
         self.wifi = WifiManager()
         NSLog("Networking::new")
         super.init()
-    }
-    
-    @objc
-    public func test() {
-        NSLog("Networking::test")
-    }
-    
-    @objc
-    public func getWeb() -> Web {
-        return web
-    }
-    
-    @objc
-    public func getServiceDiscovery() -> ServiceDiscovery {
-        return serviceDiscovery
-    }
-    
-    @objc
-    public func getWifiManager() -> WifiManager {
-        return wifi
-    }
-    
-    @objc
-    public func start(serviceType: String) {
-        NSLog("Networking::start");
-        serviceDiscovery.start(serviceType: serviceType)
-        networkingListener.onStarted()
     }
 }
 
