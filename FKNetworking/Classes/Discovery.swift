4992647b86b44ed0348a0643426ac63b0d861ce8
Jacob Lewallen <jlewallen@gmail.com>
2020-10-27 15:30:46 -0700

    Move local service to UDP port so we can tell. Some cleanup.

--- FKNetworking/Classes/Discovery.swift
+++ FKNetworking/Classes/Discovery.swift
@@ -8,6 +8,10 @@
 import Foundation
 import Network
 
+let UdpMulticastGroup = "224.1.2.3";
+let UdpPort = 22143;
+let DefaultLocalDomain = "local."
+
 protocol SimpleUDP {
     func start();
     func stop();
@@ -17,16 +21,18 @@ protocol SimpleUDP {
 class LatestSimpleUDP : SimpleUDP {
     private var networkingListener: NetworkingListener
     private var group: NWConnectionGroup?;
-    
+
     init(networkingListener: NetworkingListener) {
         self.networkingListener = networkingListener
     }
-    
+
     public func start() {
         NSLog("ServiceDiscovery::iOS 14, listening udp");
 
+        let endpoint = NWEndpoint.hostPort(host: NWEndpoint.Host(UdpMulticastGroup),
+                                           port: NWEndpoint.Port(rawValue: UInt16(UdpPort))!)
         DispatchQueue.global(qos: .background).async {
-            guard let multicast = try? NWMulticastGroup(for: [ .hostPort(host: "224.1.2.3", port: 22143) ]) else {
+            guard let multicast = try? NWMulticastGroup(for: [endpoint]) else {
                 NSLog("ServiceDiscovery: error creating group")
                 return
             }
@@ -60,13 +66,13 @@ class LatestSimpleUDP : SimpleUDP {
             }
 
             group.start(queue: .main)
-            
+
             self.group = group
-            
+
             NSLog("ServiceDiscovery: simple udp running")
         }
     }
-    
+
     public func stop() {
         guard let g = self.group else { return }
         g.cancel()
@@ -81,14 +87,14 @@ open class ServiceDiscovery : NSObject, NetServiceBrowserDelegate, NetServiceDel
     var pending: NetService?
     var simple: SimpleUDP?
     var ourselves: NetService?
-    
+
     @objc
     init(networkingListener: NetworkingListener) {
         self.networkingListener = networkingListener
         self.browser = NetServiceBrowser()
         super.init()
     }
-    
+
     @objc
     public func start(serviceTypeSearch: String, serviceNameSelf: String?, serviceTypeSelf: String?) {
         NSLog("ServiceDiscovery::starting");
@@ -96,18 +102,18 @@ open class ServiceDiscovery : NSObject, NetServiceBrowserDelegate, NetServiceDel
         pending = nil
         browser.delegate = self
         browser.stop()
-        browser.searchForServices(ofType: serviceTypeSearch, inDomain: "local.")
-        
+        browser.searchForServices(ofType: serviceTypeSearch, inDomain: DefaultLocalDomain)
+
         if ourselves == nil && serviceNameSelf != nil && serviceTypeSelf != nil {
             NSLog("ServiceDiscovery:registering self: name=%@ type=%@", serviceNameSelf!, serviceTypeSelf!)
-            ourselves = NetService(domain: "local.", type: serviceTypeSelf!, name: serviceNameSelf!, port: 80)
+            ourselves = NetService(domain: DefaultLocalDomain, type: serviceTypeSelf!, name: serviceNameSelf!, port: Int32(UdpPort))
             ourselves!.delegate = self
             ourselves!.publish()
         }
         else {
             NSLog("ServiceDiscovery:NOT registering self")
         }
-        
+
         if #available(iOS 14.00, *) {
             if simple == nil {
                 simple = LatestSimpleUDP(networkingListener: self.networkingListener)
@@ -115,32 +121,32 @@ open class ServiceDiscovery : NSObject, NetServiceBrowserDelegate, NetServiceDel
             }
         }
     }
-    
+
     @objc
     public func stop() {
         browser.stop()
     }
-    
+
     public func netService(_ sender: NetService, didNotPublish errorDict: [String : NSNumber]) {
         NSLog("ServiceDiscovery::didNotPublish: %@", sender.name)
         NSLog("ServiceDiscovery::didNotPublish: %@", errorDict)
     }
-    
+
     public func netServiceWillPublish(_ sender: NetService) {
         NSLog("ServiceDiscovery::netServiceWillPublish");
     }
-    
+
     public func netServiceDidPublish(_ sender: NetService) {
         NSLog("ServiceDiscovery::netServiceDidPublish");
     }
-    
+
     public func netServiceWillResolve(_ sender: NetService) {
         NSLog("ServiceDiscovery::netServiceWillResolve");
     }
-    
+
     public func netServiceDidResolveAddress(_ sender: NetService) {
         NSLog("ServiceDiscovery::netServiceDidResolveAddress %@ %@", sender.name, sender.hostName ?? "<none>");
-        
+
         if let serviceIp = resolveIPv4(addresses: sender.addresses!) {
             NSLog("Found IPV4: %@", serviceIp)
             networkingListener.onFoundService(service: ServiceInfo(type: sender.type, name: sender.name, host: serviceIp, port: sender.port))
@@ -149,21 +155,21 @@ open class ServiceDiscovery : NSObject, NetServiceBrowserDelegate, NetServiceDel
             NSLog("No ipv4")
         }
     }
-    
+
     public func netService(_ sender: NetService, didNotResolve errorDict: [String : NSNumber]) {
         NSLog("ServiceDiscovery::didNotResolve");
     }
-    
+
     public func netServiceBrowserWillSearch(_ browser: NetServiceBrowser) {
         NSLog("ServiceDiscovery::willSearch")
-        
+
         networkingListener.onStarted()
     }
-    
+
     public func netServiceBrowserDidStopSearch(_ browser: NetServiceBrowser) {
         NSLog("ServiceDiscovery::netServiceBrowserDidStopSearch");
     }
-    
+
     public func netServiceBrowser(_ browser: NetServiceBrowser, didNotSearch errorDict: [String : NSNumber]) {
         NSLog("ServiceDiscovery::didNotSearch");
         for (key, code) in errorDict {
@@ -171,53 +177,53 @@ open class ServiceDiscovery : NSObject, NetServiceBrowserDelegate, NetServiceDel
         }
         networkingListener.onDiscoveryFailed()
     }
-    
+
     public func netServiceBrowser(_ browser: NetServiceBrowser, didFindDomain domainString: String, moreComing: Bool) {
         NSLog("ServiceDiscovery::didFindDomain");
     }
-    
+
     public func netServiceBrowser(_ browser: NetServiceBrowser, didFind service: NetService, moreComing: Bool) {
         NSLog("ServiceDiscovery::didFindService %@ %@", service.name, service.type);
-        
+
         service.stop()
         service.delegate = self
         service.resolve(withTimeout: 5.0)
-        
+
         // TODO Do we need a queue of these?
         pending = service
     }
-    
+
     public func netServiceBrowser(_ browser: NetServiceBrowser, didRemoveDomain domainString: String, moreComing: Bool) {
         NSLog("ServiceDiscovery::didRemoveDomain");
     }
-    
+
     public func netServiceBrowser(_ browser: NetServiceBrowser, didRemove service: NetService, moreComing: Bool) {
         NSLog("ServiceDiscovery::didRemoveService %@", service.name);
         networkingListener.onLostService(service: ServiceInfo(type: service.type, name: service.name, host: "", port: 0))
     }
-    
+
     func resolveIPv4(addresses: [Data]) -> String? {
         var resolved: String?
-        
+
         for address in addresses {
             let data = address as NSData
             var storage = sockaddr_storage()
             data.getBytes(&storage, length: MemoryLayout<sockaddr_storage>.size)
-            
+
             if Int32(storage.ss_family) == AF_INET {
                 let addr4 = withUnsafePointer(to: &storage) {
                     $0.withMemoryRebound(to: sockaddr_in.self, capacity: 1) {
                         $0.pointee
                     }
                 }
-                
+
                 if let ip = String(cString: inet_ntoa(addr4.sin_addr), encoding: .ascii) {
                     resolved = ip
                     break
                 }
             }
         }
-        
+
         return resolved
     }
 }
